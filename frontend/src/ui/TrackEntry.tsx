e0bcf745c9aeaa2c820479adc58f6ea7c68fe04b
Viktor Luft <viktor.luft@freiheit.com>
2023-03-01 12:58:25 +0100

    Add resume functionality (#10)

--- frontend/src/ui/TrackEntry.tsx
+++ frontend/src/ui/TrackEntry.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { ClockAmountIcon } from '../icons/ClockIcon';
-import { IconDelete } from '../icons/icon';
+import { IconDelete, IconPlay } from '../icons/icon';
 import { Track } from '../services/Types';
 import { Category } from '../services/CategoryService';
 import { TrackService } from '../services/TrackService';
@@ -14,6 +14,9 @@ type TrackEntryProps = {
     category: Category;
     onDelete?: (ID: string) => Promise<void>;
     onChange?: (track: Track) => Promise<void>;
+    onResume?: (track: Track) => Promise<void>;
+
+    isLatestOfDescriptions?: boolean;
 };
 
 export const TrackEntry: React.FC<TrackEntryProps> = ({
@@ -21,11 +24,17 @@ export const TrackEntry: React.FC<TrackEntryProps> = ({
     track,
     onDelete,
     onChange,
+    onResume,
     category: { color, name },
     rate,
     diff,
+    isLatestOfDescriptions,
 }) => {
     const _onDelete = useMemo(() => (onDelete ? () => onDelete(ID) : undefined), [onDelete, ID]);
+    const _onResume = useMemo(
+        () => (onResume && rate && isLatestOfDescriptions ? () => onResume(track) : undefined),
+        [isLatestOfDescriptions, onResume, track, rate]
+    );
     const onChangeTime = useMemo(
         () => (onChange ? (time: Date) => onChange({ ...track, time }) : undefined),
         [onChange, track]
@@ -44,6 +53,17 @@ export const TrackEntry: React.FC<TrackEntryProps> = ({
                 onChange={onChangeDescription}
                 title={name || undefined}
             />
+            <div className={'track__resume'}>
+                {_onResume && (
+                    <button
+                        onClick={_onResume}
+                        className={'icon-button'}
+                        title={'resume track'}
+                        aria-label={'resume track'}>
+                        <IconPlay />
+                    </button>
+                )}
+            </div>
             <div className={'track__rate'} title={rate ? Math.floor(rate * 100) + '%' : undefined}>
                 {rate && <ClockAmountIcon rate={rate} />}
             </div>
--- frontend/src/ui/Tracks.tsx
+++ frontend/src/ui/Tracks.tsx
@@ -59,24 +59,31 @@ type TracksProps = {
     extendedTracks: ExtendedTracks;
     onDelete?: (ID: string) => Promise<void>;
     onChange?: (track: Track) => Promise<void>;
+    onResume?: (track: Track) => Promise<void>;
 };
 
 export const Tracks: React.FC<TracksProps> = ({
     extendedTracks: { tracks, trackDiffs, trackRates, categories },
     onDelete,
     onChange,
-}) => (
-    <div className={'tracks'}>
-        {tracks.map((track, i) => (
-            <TrackEntry
-                track={track}
-                category={categories[i]}
-                key={track.ID}
-                rate={trackRates[i]}
-                diff={trackDiffs[i]}
-                onDelete={onDelete}
-                onChange={onChange}
-            />
-        ))}
-    </div>
-);
+    onResume,
+}) => {
+    const allDescriptions = tracks.map(({ description }) => description);
+    return (
+        <div className={'tracks'}>
+            {tracks.map((track, i) => (
+                <TrackEntry
+                    track={track}
+                    category={categories[i]}
+                    key={track.ID}
+                    rate={trackRates[i]}
+                    diff={trackDiffs[i]}
+                    onDelete={onDelete}
+                    onChange={onChange}
+                    onResume={onResume}
+                    isLatestOfDescriptions={allDescriptions.lastIndexOf(track.description) === i}
+                />
+            ))}
+        </div>
+    );
+};
