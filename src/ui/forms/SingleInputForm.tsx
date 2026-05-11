83d173caf0c994b0419bb1e408b491617488971d
Viktor Luft <viktor.luft@freiheit.com>
2026-05-11 18:48:34 +0200

    Fix input cursor jump issue

--- src/ui/forms/SingleInputForm.tsx
+++ src/ui/forms/SingleInputForm.tsx
@@ -1,17 +1,5 @@
-import { type KeyboardEvent, useRef, useState } from "react";
-import { FormRef, Morfi, MorfiData } from "morfi";
-import { FormInput } from "../base/Input";
-import { useSafeState } from "../hooks/useSafeState";
-
-type SingleInputFormValues = { value: string };
-type SingleInputFormData = MorfiData<SingleInputFormValues>;
-
-const VALIDATION = {
-  value: {
-    onChange: (value?: string) =>
-      value ? undefined : { id: "At least one character required" },
-  },
-};
+import { type KeyboardEvent, useState } from "react";
+import { Input } from "../base/Input";
 
 export type SingleInputFormProps = {
   value?: string;
@@ -26,34 +14,30 @@ export const SingleInputForm: React.FC<SingleInputFormProps> = ({
   submitOnBlur,
   inputName,
 }) => {
-  const [initialData] = useState(() => Morfi.initialData({ value }));
-  const [data, setData] = useSafeState<SingleInputFormData>(initialData);
-  const { fields, Form } = Morfi.useForm<SingleInputFormValues>();
-  const ref = useRef<FormRef<SingleInputFormValues> | null>(null);
+  const [input, setInput] = useState(value);
+
+  const onSubmit = () => {
+    if (input) {
+      onChange(input);
+      setInput(value);
+    }
+  };
   const onEscape = () => {
-    if (submitOnBlur) ref.current?.submit();
+    if (submitOnBlur) onSubmit();
   };
   const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter") onSubmit();
     if (e.code === "Escape") onEscape();
   };
 
   return (
-    <Form
-      ref={ref}
-      onChange={setData}
-      data={data}
-      onSubmit={({ value }) => onChange(value)}
-      onSubmitFinished={() => setData(initialData)}
-      validation={VALIDATION}
-    >
-      <FormInput
-        name={inputName}
-        autoFocus
-        onBlur={onEscape}
-        field={fields.value}
-        onKeyDown={onKeyDown}
-      />
-      <button style={{ display: "none" }} />
-    </Form>
+    <Input
+      value={input}
+      onChange={setInput}
+      name={inputName}
+      autoFocus
+      onBlur={onEscape}
+      onKeyDown={onKeyDown}
+    />
   );
 };
--- src/ui/forms/TimeForm.tsx
+++ src/ui/forms/TimeForm.tsx
@@ -1,8 +1,5 @@
-import React, { type KeyboardEvent, useRef, useState } from "react";
-import { FormRef, Morfi, MorfiData } from "morfi";
-import { FormTimeInput } from "../base/Input";
-
-type TimeFormValues = { time: Date };
+import React, { type KeyboardEvent, useState } from "react";
+import { TimeInput } from "../base/Input";
 
 export type TimeFormProps = {
   time: Date;
@@ -10,34 +7,30 @@ export type TimeFormProps = {
 };
 
 export const TimeForm: React.FC<TimeFormProps> = ({ time, onChange }) => {
-  const [data, setData] = useState<MorfiData<TimeFormValues>>(
-    Morfi.initialData({ time }),
-  );
-  const { fields, Form } = Morfi.useForm<TimeFormValues>();
+  const [value, setValue] = useState(time);
 
-  const ref = useRef<FormRef<TimeFormValues> | null>(null);
+  const onSubmit = () => {
+    if (value) {
+      onChange(value);
+      setValue(time);
+    }
+  };
   const onEscape = () => {
-    ref.current?.submit();
+    onSubmit();
   };
   const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter") onSubmit();
     if (e.code === "Escape") onEscape();
   };
 
   return (
-    <Form
-      onChange={setData}
-      data={data}
-      onSubmit={({ time }) => onChange(time)}
-      ref={ref}
-    >
-      <FormTimeInput
-        name={"track-time"}
-        autoFocus
-        onBlur={onEscape}
-        field={fields.time}
-        onKeyDown={onKeyDown}
-      />
-      <button style={{ display: "none" }} />
-    </Form>
+    <TimeInput
+      value={value}
+      onChange={setValue}
+      name={"track-time"}
+      autoFocus
+      onBlur={onEscape}
+      onKeyDown={onKeyDown}
+    />
   );
 };
